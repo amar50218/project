113c718eea887b024e64468b83b6de03a54cac08
amar50218 <amar50218@gmail.com>
2018-07-25 14:38:46 +0530

    Add files via upload

---
+++ file.bat
@@ -0,0 +1,7 @@
+mkidr C:\Program Files (x86)\Jenkins\workspace\file
+cd C:\Program Files (x86)\Jenkins\workspace\file
+git clone https://github.com/amar50218/test_1.git 
+echo smothing >text.txt
+git add .
+git commit -m "file"
+gti push origin master
